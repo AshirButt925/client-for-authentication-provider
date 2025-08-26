95a693dd68f533ff8bf1d87364b8499d4d7f347b
Ashir Ali Butt <ashirbutt925@gmail.com>
2025-08-26 15:50:15 +0500

    feat: add Laravel 5.3-8.x route syntax compatibility

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use ClientForAuthenticationProvider\Http\Controllers\ClientAuthController;
 
 Route::group([
     'prefix' => config('client-for-authentication-provider.route_prefix', 'api'),
     'middleware' => config('client-for-authentication-provider.route_middleware', 'api'),
 ], function () {
-    Route::post('/create-or-login-user', [ClientAuthController::class, 'miniAppCreateOrLoginUser']);
+    Route::post('/create-or-login-user', 'ClientForAuthenticationProvider\Http\Controllers\ClientAuthController@miniAppCreateOrLoginUser');
 });
